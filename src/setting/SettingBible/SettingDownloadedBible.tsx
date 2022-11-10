dddefcecdabbff495b95c12067d650df8f348a01
raksa <eng.raksa@gmail.com>
2022-11-09 21:45:28 -0500

    improved

--- src/setting/SettingBible/SettingDownloadedBible.tsx
+++ src/setting/SettingBible/SettingDownloadedBible.tsx
@@ -1,16 +1,6 @@
-import ToastEventListener from '../../event/ToastEventListener';
+import DownloadedBibleItem from './DownloadedBibleItem';
 import { BibleListType } from './helpers';
-import { OnlineBibleItem } from './SettingOnlineBible';
-import {
-    toBiblePath,
-} from '../../server/bible-helpers/bibleInfoHelpers';
-import {
-    fsCheckDirExist,
-    fsDeleteDir,
-} from '../../server/fileHelper';
-import {
-    BibleMinimalInfoType,
-} from '../../server/bible-helpers/bibleDownloadHelpers';
+import OnlineBibleItem from './OnlineBibleItem';
 
 export default function SettingDownloadedBible({
     onlineBibleInfoList,
@@ -84,55 +74,5 @@ export default function SettingDownloadedBible({
                 </>)}
             </ul>
         </div>
-    );
-}
-
-function DownloadedBibleItem({
-    bibleInfo,
-    onDeleted,
-    onUpdate,
-}: {
-    bibleInfo: BibleMinimalInfoType & { isUpdatable: boolean },
-    onDeleted: () => void,
-    onUpdate: () => void,
-}) {
-    const { key, title } = bibleInfo;
-    const onDeleteHandler = async () => {
-        try {
-            const bibleDestination = await toBiblePath(key);
-            if (bibleDestination !== null &&
-                await fsCheckDirExist(bibleDestination)) {
-                await fsDeleteDir(bibleDestination);
-            }
-            onDeleted();
-        } catch (error: any) {
-            ToastEventListener.showSimpleToast({
-                title: 'Deleting',
-                message: error.message,
-            });
-        }
-    };
-    return (
-        <li className='list-group-item'>
-            <div>
-                <span>{title} ({key})</span>
-                <div className='float-end'>
-                    <div className='btn-group'>
-                        <button className='btn btn-danger'
-                            onClick={onDeleteHandler}>
-                            Delete
-                        </button>
-                        {bibleInfo.isUpdatable && (<button
-                            className='btn btn-warning'
-                            onClick={() => {
-                                onUpdate();
-                            }}>
-                            Update
-                        </button>)
-                        }
-                    </div>
-                </div>
-            </div>
-        </li>
     );
 }
--- src/setting/SettingBible/SettingOnlineBible.tsx
+++ src/setting/SettingBible/SettingOnlineBible.tsx
@@ -1,12 +1,5 @@
-import { useState } from 'react';
-import ToastEventListener from '../../event/ToastEventListener';
-import appProvider from '../../server/appProvider';
-import {
-    BibleMinimalInfoType,
-    downloadBible,
-    extractDownloadedBible,
-} from '../../server/bible-helpers/bibleDownloadHelpers';
 import { BibleListType } from './helpers';
+import OnlineBibleItem from './OnlineBibleItem';
 
 export default function SettingOnlineBible({
     downloadedBibleInfoList,
@@ -68,73 +61,5 @@ export default function SettingOnlineBible({
                 })}
             </ul>
         </div>
-    );
-}
-
-export function OnlineBibleItem({
-    bibleInfo,
-    onDownloaded,
-}: {
-    bibleInfo: BibleMinimalInfoType,
-    onDownloaded: () => void,
-}) {
-    const { key, title } = bibleInfo;
-    const [
-        downloadingProgress,
-        setDownloadingProgress,
-    ] = useState<number | null>(null);
-    const onDownloadHandler = () => {
-        setDownloadingProgress(0);
-        downloadBible({
-            bibleInfo,
-            options: {
-                onStart: (fileSize) => {
-                    ToastEventListener.showSimpleToast({
-                        title: `Start downloading ${key}`,
-                        message: `Total file size ${fileSize}mb`,
-                    });
-                },
-                onProgress: (percentage) => {
-                    setDownloadingProgress(percentage);
-                },
-                onDone: async (error, filePath) => {
-                    if (error) {
-                        appProvider.appUtils.handleError(error);
-                    } else {
-                        await extractDownloadedBible(filePath as string);
-                        onDownloaded();
-                    }
-                    setDownloadingProgress(null);
-                },
-            },
-        });
-    };
-    return (
-        <li className='list-group-item'>
-            <div className='w-100'>
-                <span>{title} ({key})</span>
-                {downloadingProgress === null ?
-                    (<div className='float-end'>
-                        <button className='btn btn-info'
-                            onClick={onDownloadHandler}>
-                            Download
-                            <i className='bi bi-cloud-arrow-down' />
-                        </button>
-                    </div>) : (<div>
-                        <div className='progress'>
-                            <div className={'progress-bar progress-bar-striped '
-                                + 'progress-bar-animated'}
-                                role='progressbar'
-                                aria-valuenow={downloadingProgress * 100}
-                                aria-valuemin={0}
-                                aria-valuemax={100}
-                                style={{
-                                    width: `${downloadingProgress * 100}%`,
-                                }} />
-                        </div>
-                    </div>)
-                }
-            </div>
-        </li>
     );
 }
