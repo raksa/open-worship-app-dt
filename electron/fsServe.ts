492e30afde5c3d6a59e3c405188e6042d27d3d72
raksa <eng.raksa@gmail.com>
2023-08-29 08:58:46 -0400

    improved fsServe

--- electron/fsServe.ts
+++ electron/fsServe.ts
@@ -1,6 +1,6 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import { BrowserWindow, app, net, protocol } from 'electron';
+import { app, net, protocol } from 'electron';
 
 const indexHtml = 'index.html';
 export const scheme = 'http';
@@ -16,55 +16,17 @@ function toFileFullPath(filePath: string) {
     return null;
 }
 
-const mapper = {
-    '.owb': 'application/owb',
-    '.apng': 'image/apng',
-    '.avif': 'image/avif',
-    '.gif': 'image/gif',
-    '.jpg"': 'image/jpeg',
-    '.jpeg"': 'image/jpeg',
-    '.jfif"': 'image/jpeg',
-    '.pjpeg"': 'image/jpeg',
-    '.pjp': 'image/jpeg',
-    '.png': 'image/png',
-    '.svg': 'image/svg+xml',
-    '.webp': 'image/webp',
-    '.owl': 'application/owl',
-    '.owp': 'application/owp',
-    '.ows': 'application/ows',
-    '.mp4': 'video/mp4',
-    '.webm': 'video/webm',
-    '.mov': 'video/quicktime',
-    '.js': 'text/javascript',
-    '.css': 'text/css',
-    '.json': 'application/json',
-    '.jpg': 'image/jpg',
-    '.wav': 'audio/wav',
-    '.mp3': 'audio/mp3',
-    '.pdf': 'application/pdf',
-    '.doc': 'application/msword',
-    '.eot': 'appliaction/vnd.ms-fontobject',
-    '.ttf': 'aplication/font-sfnt',
-    '.woff': 'application/font-woff',
-    '.woff2': 'application/font-woff2',
-    '.wasm': 'application/wasm',
-};
-function contentType(filePath: string) {
-    const extname = path.extname(filePath);
-    return mapper[extname] ?? 'text/html';
+function genFilePathUrl(dirPath: string, url: string) {
+    url = decodeURIComponent((new URL(url)).pathname);
+    let filePath = path.join(dirPath, url);
+    filePath = toFileFullPath(filePath) ?? path.join(dirPath, indexHtml);
+    return `file://${filePath}`;
 }
 
 async function handler(dirPath: string, request: GlobalRequest) {
-    if (!request.url.startsWith(rootUrl)) {
-        return net.fetch(request);
-    }
-    const url = decodeURIComponent((new URL(request.url)).pathname);
-    let filePath = path.join(dirPath, url);
-    const fallback = path.join(dirPath, indexHtml);
-    filePath = toFileFullPath(filePath) ?? fallback;
-    return new Response(fs.readFileSync(filePath), {
-        headers: { 'content-type': contentType(filePath) },
-    });
+    const isLocal = request.url.startsWith(rootUrl);
+    const urlPath = isLocal ? genFilePathUrl(dirPath, request.url) : request.url;
+    return net.fetch(urlPath);
 };
 
 let registered = false;
@@ -74,8 +36,8 @@ export function registerScheme() {
             return;
         }
         registered = true;
-        const directory = path.resolve(app.getAppPath(), 'dist');
-        protocol.handle(scheme, handler.bind(null, directory));
+        const dirPath = path.resolve(app.getAppPath(), 'dist');
+        protocol.handle(scheme, handler.bind(null, dirPath));
     };
     return registerHandler;
 };
