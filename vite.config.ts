3393c071c4d77d2529f292286f72c28d0c059785
raksa <eng.raksa@gmail.com>
2023-11-24 12:17:58 -0500

    added finder.html

--- vite.config.ts
+++ vite.config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
     },
     build: {
         rollupOptions: {
-            input: ['index.html', 'present.html'],
+            input: ['index.html', 'present.html', 'finder.html'],
         },
     },
 });
