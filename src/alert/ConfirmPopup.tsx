fe620ca63253e6e6884422479b478e990e419043
raksa <eng.raksa@gmail.com>
2024-12-11 20:42:37 -0500

    refactor: improve ConfirmPopup event handling and rename functions for clarity

--- src/alert/ConfirmPopup.tsx
+++ src/alert/ConfirmPopup.tsx
@@ -10,13 +10,20 @@ import { useKeyboardRegistering } from '../event/KeyboardEventListener';
 export default function ConfirmPopup({ data }: Readonly<{
     data: ConfirmDataType,
 }>) {
-    const handClose = () => {
+    const handleClosing = () => {
         data.onConfirm(false);
+        closeAlert();
+    };
+    const handlerOkClicking = () => {
+        data.onConfirm(true);
         closeAlert();
     };
     useKeyboardRegistering([{ key: 'Escape' }], (event) => {
         event.preventDefault();
-        handClose();
+        handleClosing();
+    });
+    useKeyboardRegistering([{ key: 'Enter' }], () => {
+        handlerOkClicking();
     });
     return (
         <PrimitiveModal>
@@ -28,7 +35,7 @@ export default function ConfirmPopup({ data }: Readonly<{
                         {data.title}
                     </>
                 )}
-                    onClose={handClose}
+                    onClose={handleClosing}
                 />
                 <div className='card-body d-flex flex-column'>
                     <div className='p-2 flex-fill app-selectable-text'
@@ -39,15 +46,12 @@ export default function ConfirmPopup({ data }: Readonly<{
                     <div className='btn-group float-end'>
                         <button type='button'
                             className='btn btn-sm'
-                            onClick={handClose}>
+                            onClick={handleClosing}>
                             Cancel
                         </button>
                         <button type='button'
                             className='btn btn-sm btn-info'
-                            onClick={() => {
-                                data.onConfirm(true);
-                                closeAlert();
-                            }}>
+                            onClick={handlerOkClicking}>
                             Ok
                         </button>
                     </div>
