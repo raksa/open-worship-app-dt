ece53ffc0bfbfc8bbc522d4d8a447e5e41f4c485
raksa <eng.raksa@gmail.com>
2025-05-24 21:31:39 -0400

    refactor: extract styling logic into useStylingColor and useStylingFontSize hooks

--- src/_screen/preview/TextShadow.tsx
+++ src/_screen/preview/TextShadow.tsx
@@ -3,24 +3,77 @@ import './TextShadow.scss';
 import ScreenBibleManager from '../managers/ScreenBibleManager';
 import { AppColorType } from '../../others/color/colorHelpers';
 import { useAppEffect } from '../../helper/debuggerHelpers';
+import ReactDOMServer from 'react-dom/server';
+import { useMemo } from 'react';
+import { useStylingColor } from './stylingHelpers';
 
-function genShadow1(color1: string, color2: string) {
-    const style =
-        `color: ${color1}; text-shadow: 2px 2px 0 ${color2}, ` +
-        `2px -2px 0 ${color2}, -2px 2px 0 ${color2}, -2px -2px 0 ` +
-        `${color2}, 2px 0px 0 ${color2}, 0px 2px 0 ${color2}, -2px 0px ` +
-        `0 ${color2}, 0px -2px 0 ${color2};`;
-    return `
-    <div class="ow-outline-demo" style="color: #ffffff">
-        None
-    </div>
-    <div class="ow-outline-demo" style="${style}">
-        Outline1
-    </div>
-`;
+function genShadow(prefix: string, color1: string, color2: string) {
+    const htmlString = ReactDOMServer.renderToStaticMarkup(
+        <>
+            <div
+                className="ow-outline-demo"
+                style={{
+                    color: color1,
+                    textShadow:
+                        `2px 2px 0 ${color2}, ` +
+                        `2px -2px 0 ${color2}, ` +
+                        `-2px 2px 0 ${color2}, ` +
+                        `-2px -2px 0 ${color2}, ` +
+                        `2px 0px 0 ${color2}, ` +
+                        `0px 2px 0 ${color2}, ` +
+                        `-2px 0px 0 ${color2}, ` +
+                        `0px -2px 0 ${color2}`,
+                }}
+            >
+                {prefix}:Outline1
+            </div>
+            <div
+                className="ow-outline-demo"
+                style={{
+                    color: color1,
+                    textShadow:
+                        `2px 2px 0 ${color2}, ` +
+                        `2px -2px 0 ${color2}, ` +
+                        `-2px 2px 0 ${color2}, ` +
+                        `-2px -2px 0 ${color2}, ` +
+                        `2px 0px 0 ${color2}, ` +
+                        `0px 2px 0 ${color2}, ` +
+                        `-2px 0px 0 ${color2}, ` +
+                        `0px -2px 0 ${color2}, ` +
+                        `1px 1px 5px #000, ` +
+                        `-1px -1px 5px #000`,
+                }}
+            >
+                {prefix}:Outline2
+            </div>
+            <div
+                className="ow-outline-demo"
+                style={{
+                    color: color1,
+                    textShadow:
+                        `2px 2px 0 ${color2}, ` +
+                        `2px -2px 0 ${color2}, ` +
+                        `-2px 2px 0 ${color2}, ` +
+                        `-2px -2px 0 ${color2}, ` +
+                        `2px 0px 0 ${color2}, ` +
+                        `0px 2px 0 ${color2}, ` +
+                        `-2px 0px 0 ${color2}, ` +
+                        `0px -2px 0 ${color2}, ` +
+                        `1px 1px 5px #000, ` +
+                        `-1px -1px 5px #000,` +
+                        `1px -1px 5px #000,` +
+                        `-1px 1px 5px #000`,
+                }}
+            >
+                {prefix}:Outline3
+            </div>
+        </>,
+    );
+    return htmlString;
 }
 
 export default function TextShadow() {
+    const [color] = useStylingColor();
     useAppEffect(() => {
         const divList =
             document.querySelectorAll<HTMLDivElement>('.ow-outline-demo');
@@ -40,14 +93,23 @@ export default function TextShadow() {
             });
         };
     }, []);
-    const color1 = '#ffffff';
-    const color2 = '#4054b2';
+    const htmlColorText = useMemo(() => {
+        let text = `
+            <div class="ow-outline-demo" style="color:${color}">None</div>
+        `;
+        text += '<hr/>' + genShadow('G1', color, '#4054b2');
+        text += '<hr/>' + genShadow('G2', color, '#000000');
+        return text;
+    }, [color]);
     return (
-        <div className="card-body">
+        <div
+            className="card-body"
+            style={{ maxHeight: '200px', overflowY: 'auto' }}
+        >
             <div
                 className="text-shadow"
                 dangerouslySetInnerHTML={{
-                    __html: genShadow1(color1, color2),
+                    __html: htmlColorText,
                 }}
             ></div>
         </div>
