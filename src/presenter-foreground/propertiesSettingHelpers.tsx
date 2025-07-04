017b5c0a52ada8229db078660dfa86ad34b4e803
raksa <eng.raksa@gmail.com>
2025-07-04 11:44:01 -0400

    feat: add toggle functionality for properties settings; integrate useStateSettingBoolean for improved state management

--- src/presenter-foreground/propertiesSettingHelpers.tsx
+++ src/presenter-foreground/propertiesSettingHelpers.tsx
@@ -1,5 +1,6 @@
 import {
     getSetting,
+    useStateSettingBoolean,
     useStateSettingNumber,
     useStateSettingString,
 } from '../helper/settingHelpers';
@@ -142,6 +143,7 @@ function PropertiesSettingComp({
     setFontSize,
     roundSizePixel,
     setRoundSizePixel,
+    target,
 }: Readonly<{
     alignmentData: string;
     setAlignmentData: (data: string) => void;
@@ -160,135 +162,168 @@ function PropertiesSettingComp({
     setFontSize: (value: number) => void;
     roundSizePixel: number;
     setRoundSizePixel: (value: number) => void;
+    target: string;
 }>) {
-    return (
-        <div className="d-flex flex-wrap p-1 align-items-center">
-            <div className="m-1">
-                <SlideEditorToolAlignComp
-                    data={JSON.parse(alignmentData)}
-                    onData={(data) => {
-                        const oldData = JSON.parse(alignmentData);
-                        setAlignmentData(
-                            JSON.stringify({
-                                ...oldData,
-                                ...data,
-                            }),
-                        );
-                    }}
-                />
-            </div>
-            <div
-                className="d-flex input-group m-1"
-                style={{ width: '330px', height: '35px' }}
-            >
-                <div className="input-group-text">Offset X:</div>
-                <input
-                    type="number"
-                    className="form-control"
-                    value={widgetOffsetX}
-                    onChange={(event) => {
-                        setWidgetOffsetX(parseInt(event.target.value) || 0);
-                    }}
-                />
-                <div className="input-group-text">Offset Y:</div>
-                <input
-                    type="number"
-                    className="form-control"
-                    value={widgetOffsetY}
-                    onChange={(event) => {
-                        setWidgetOffsetY(parseInt(event.target.value) || 0);
-                    }}
-                />
-            </div>
-            <div className="d-flex app-border-white-round m-1">
-                `Width:
-                <AppRangeComp
-                    value={widgetWidthPercentage}
-                    title="`Width (%)"
-                    setValue={setWidgetWidthPercentage}
-                    defaultSize={{
-                        size: widgetWidthPercentage,
-                        min: 1,
-                        max: 100,
-                        step: 1,
-                    }}
-                    isShowValue
-                />
-            </div>
-            <div className="d-flex app-border-white-round m-1">
-                `Opacity:
-                <AppRangeComp
-                    value={opacityPercentage}
-                    title="`Opacity (%)"
-                    setValue={setOpacityPercentage}
-                    defaultSize={{
-                        size: opacityPercentage,
-                        min: 0,
-                        max: 100,
-                        step: 1,
-                    }}
-                    isShowValue
-                />
-            </div>
-            <div
-                className="d-flex app-border-white-round m-1"
-                style={{
-                    opacity: roundSizePixel > 0 ? 0.5 : 1,
+    const [isOpened, setIsOpened] = useStateSettingBoolean(
+        `foreground-${target}-show-properties-setting`,
+        false,
+    );
+    if (!isOpened) {
+        return (
+            <button
+                className="btn btn-sm btn-outline-secondary"
+                onClick={() => {
+                    setIsOpened(true);
                 }}
             >
-                `Round Size %:
-                <AppRangeComp
-                    value={roundPercentage}
-                    title={
-                        roundSizePixel > 0
-                            ? 'Set round size pixel to 0 to use this'
-                            : '`Round (%)'
-                    }
-                    setValue={setRoundPercentage}
-                    defaultSize={{
-                        size: roundPercentage,
-                        min: 0,
-                        max: 100,
-                        step: 1,
-                    }}
-                    isShowValue
-                />
-            </div>
-            <div
-                className="d-flex input-group m-1"
-                style={{ width: '260px', height: '35px' }}
+                <i className="bi bi-chevron-right" />
+                <i className="bi bi-gear" />
+                {' `Properties'}
+            </button>
+        );
+    }
+    return (
+        <div>
+            <button
+                className="btn btn-sm btn-outline-secondary"
+                onClick={() => {
+                    setIsOpened(false);
+                }}
             >
-                <div className="input-group-text">`Round size pixel:</div>
-                <input
-                    type="number"
-                    className="form-control"
-                    value={roundSizePixel}
-                    onChange={(event) => {
-                        setRoundSizePixel(parseInt(event.target.value) || 0);
-                    }}
-                />
-                <div className="input-group-text">px</div>
-            </div>
-            {isFontSize ? (
+                <i className="bi bi-chevron-down" />
+                <i className="bi bi-gear" />
+                {' `Properties'}
+            </button>
+            <div className="d-flex flex-wrap p-1 align-items-center app-inner-shadow">
+                <div className="m-1">
+                    <SlideEditorToolAlignComp
+                        data={JSON.parse(alignmentData)}
+                        onData={(data) => {
+                            const oldData = JSON.parse(alignmentData);
+                            setAlignmentData(
+                                JSON.stringify({
+                                    ...oldData,
+                                    ...data,
+                                }),
+                            );
+                        }}
+                    />
+                </div>
                 <div
                     className="d-flex input-group m-1"
-                    style={{ width: '220px', height: '35px' }}
+                    style={{ width: '330px', height: '35px' }}
                 >
-                    <div className="input-group-text">Font Size:</div>
+                    <div className="input-group-text">Offset X:</div>
                     <input
                         type="number"
                         className="form-control"
-                        value={fontSize}
+                        value={widgetOffsetX}
                         onChange={(event) => {
-                            setFontSize(
-                                parseInt(event.target.value) ||
-                                    DEFAULT_FONT_SIZE,
+                            setWidgetOffsetX(parseInt(event.target.value) || 0);
+                        }}
+                    />
+                    <div className="input-group-text">Offset Y:</div>
+                    <input
+                        type="number"
+                        className="form-control"
+                        value={widgetOffsetY}
+                        onChange={(event) => {
+                            setWidgetOffsetY(parseInt(event.target.value) || 0);
+                        }}
+                    />
+                </div>
+                <div className="d-flex app-border-white-round m-1">
+                    `Width:
+                    <AppRangeComp
+                        value={widgetWidthPercentage}
+                        title="`Width (%)"
+                        setValue={setWidgetWidthPercentage}
+                        defaultSize={{
+                            size: widgetWidthPercentage,
+                            min: 1,
+                            max: 100,
+                            step: 1,
+                        }}
+                        isShowValue
+                    />
+                </div>
+                <div className="d-flex app-border-white-round m-1">
+                    `Opacity:
+                    <AppRangeComp
+                        value={opacityPercentage}
+                        title="`Opacity (%)"
+                        setValue={setOpacityPercentage}
+                        defaultSize={{
+                            size: opacityPercentage,
+                            min: 0,
+                            max: 100,
+                            step: 1,
+                        }}
+                        isShowValue
+                    />
+                </div>
+                <div
+                    className="d-flex app-border-white-round m-1"
+                    style={{
+                        opacity: roundSizePixel > 0 ? 0.5 : 1,
+                    }}
+                >
+                    `Round Size %:
+                    <AppRangeComp
+                        value={roundPercentage}
+                        title={
+                            roundSizePixel > 0
+                                ? 'Set round size pixel to 0 to use this'
+                                : '`Round (%)'
+                        }
+                        setValue={setRoundPercentage}
+                        defaultSize={{
+                            size: roundPercentage,
+                            min: 0,
+                            max: 100,
+                            step: 1,
+                        }}
+                        isShowValue
+                    />
+                </div>
+                <div
+                    className="d-flex input-group m-1"
+                    style={{ width: '260px', height: '35px' }}
+                >
+                    <div className="input-group-text">`Round size pixel:</div>
+                    <input
+                        type="number"
+                        className="form-control"
+                        value={roundSizePixel}
+                        onChange={(event) => {
+                            setRoundSizePixel(
+                                parseInt(event.target.value) || 0,
                             );
                         }}
                     />
                     <div className="input-group-text">px</div>
                 </div>
-            ) : null}
+                {isFontSize ? (
+                    <div
+                        className="d-flex input-group m-1"
+                        style={{ width: '220px', height: '35px' }}
+                    >
+                        <div className="input-group-text">Font Size:</div>
+                        <input
+                            type="number"
+                            className="form-control"
+                            value={fontSize}
+                            onChange={(event) => {
+                                setFontSize(
+                                    parseInt(event.target.value) ||
+                                        DEFAULT_FONT_SIZE,
+                                );
+                            }}
+                        />
+                        <div className="input-group-text">px</div>
+                    </div>
+                ) : null}
+            </div>
         </div>
     );
 }
@@ -425,6 +460,7 @@ export function useForegroundPropsSetting({
                 setFontSize={setFontSize1}
                 roundSizePixel={roundSizePixel}
                 setRoundSizePixel={setRoundSizePixel1}
+                target={prefix}
             />
         ),
     };
